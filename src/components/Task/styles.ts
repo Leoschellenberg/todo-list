e1c5831e5c3697b6d62868a7656203a83fb1cec0
Leonardo Martins Schellenberg <leonardo.schellenberg@hotmail.com>
2023-05-18 17:40:48 -0300

    fix: Correcao name component

---
+++ src/components/Task/styles.ts
@@ -0,0 +1,15 @@
+import styled, { css } from "styled-components";
+
+export const Wrapper = styled.section`
+  ${() => css``}
+`;
+
+export const Header = styled.header`
+  ${() => css``}
+`;
+export const TotalTasks = styled.h2`
+  ${() => css``}
+`;
+export const FinishedTasks = styled.h2`
+  ${() => css``}
+`;
